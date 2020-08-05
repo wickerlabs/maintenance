d16ebdef99e8a0e1b904698b5d88c4de8b261dbe
Walker Leite <walker@praiseweb.com.br>
2020-08-05 15:03:01 -0300

    remove index cat

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,4 @@ sed -i "s/{{headline}}/$(echo ${HEADLINE})/g" index.html
 sed -i "s/{{team_name}}/$(echo ${TEAM_NAME})/g" index.html
 sed -i "s/{{title}}/$(echo ${TITLE})/g" index.html
 
-cat index.html
-
 while true;  do {  echo -e "HTTP/1.1 ${RESPONSE_CODE}\r\n"; echo "$(cat index.html)"; } | nc -lvp  "$PORT";  done
